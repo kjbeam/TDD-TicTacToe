1630411b32eba7e4b0e24b902850b8546dee213b
Beam, Kevin <KBeam@lb.com>
2018-08-21 15:04:27 -0400

    Modify TicTacToe.java
    -Change from two if statements to an else if
    -Change access to package private

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -3,13 +3,12 @@ package com.packtpublishing.tddjava.ch03tictactoe;
 /**
  * @author kbeam Date:    8/21/2018 Time:    10:52 AM
  */
-public class TicTacToe {
+class TicTacToe {
 
-	public void play(int x, int y) {
+void play(int x, int y) {
 		if (x > 3 || x < 1) {
 			throw new RuntimeException("X is outside of the board");
-		}
-		if (y > 3 || y < 1) {
+		} else if (y > 3 || y < 1) {
 			throw new RuntimeException("Y is outside of the board");
 		}
 	}
